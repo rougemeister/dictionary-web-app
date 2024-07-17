c1c8d9bf8a2eb1a6381eec557fedbe52e3279847
rougemeister <rougemeister1@gmail.com>
2024-07-17 15:38:37 +0000

    Created components and basic styles

---
+++ src/app/toggler/toggler.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggler',
+  standalone: true,
+  imports: [],
+  templateUrl: './toggler.component.html',
+  styleUrl: './toggler.component.scss'
+})
+export class TogglerComponent {
+
+}
