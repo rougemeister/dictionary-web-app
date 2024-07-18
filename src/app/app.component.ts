8ce43bdf0ee07bd50fc32dddc45ae2a9fc479272
rougemeister <rougemeister1@gmail.com>
2024-07-18 03:28:54 +0000

    Added style to card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { SearchComponent } from './search/search.component';
+import { CardComponent } from "./card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, SearchComponent],
+  imports: [RouterOutlet, NavComponent, SearchComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
